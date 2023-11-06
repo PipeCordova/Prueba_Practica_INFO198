0955d800d4578d18fbecdf262503ba59958ca3d7
SebaMonte07 <seb.monte17@gmail.com>
2023-11-05 21:48:30 -0300

    Avance p2

--- Prueba02/src/frontend.cpp
+++ Prueba02/src/frontend.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <unistd.h> //getpid
 
 using namespace std;
 
 int main(int argc, char const *argv[]){
-    cout << "Hola mundo!" << endl; 
-    return EXIT_SUCCESS;
+    bool flg = true;
+    int topk = 5;
+    while (flg){
+        int clearResult = system("clear");
+        cout << "### Buscador de indice invertido (pid = "<< getpid() << ") ###\n\n";
+        cout << "- Los topk documentos serán = " << topk << "\n";
+        cout << "- Escriba texto a buscar: ";
+        string frase;
+        getline(cin, frase);
+    }
 }
